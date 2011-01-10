83019929784fe927bf6fcadd6beb64e27813b54c
dorkitude <kyle@kylewild.com>
2011-01-09 19:00:14 -0800

    test for the ruby version

---
+++ test.rb
@@ -0,0 +1,15 @@
+require './tagalog.rb'
+
+
+Tagalog::log('my message', [:nice, :hello?])
+hash = {
+  :chunky => 'bacon!!!!!!!',
+  :what? => 13511,
+}
+Tagalog::log(hash, [:tag1, :tag2])
+
+
+Tagalog::log(hash, :off)
+
+
+Tagalog::log('forced logging message', :force)
