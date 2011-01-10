2a199ec61571917dee453276f6db094c9a20c67c
dorkitude <kyle@kylewild.com>
2011-01-09 22:21:58 -0600

    add comments to ruby tests

--- test.rb
+++ test.rb
@@ -1,7 +1,16 @@
 require './tagalog.rb'
 
 
+# force a tag type exception:
+# Tagalog::log("message", {:unsupported => 'tag type'})
+
+# force a message type exception:
+# Tagalog::log(Time, :tag)
+
+# test array of tags
 Tagalog::log('my message', [:nice, :hello?])
+
+# test dictionary inputs
 hash = {
   :chunky => 'bacon!!!!!!!',
   :what? => 13511,
@@ -9,12 +18,22 @@ hash = {
 Tagalog::log(hash, [:tag1, :tag2])
 
 
+# test a tag that's off
 Tagalog::log(hash, :off)
 
-
+# test a tag that's on
 Tagalog::log('forced logging message', :force)
+
+
+# test default case
+Tagalog::log('untagged message')
+
+
+
 
 
 # TODO test for type-checking message
 
 # TODO test for type-checking tagging
+
+
