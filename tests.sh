5410fb7624fd9d2a8a00213fb0e41f53db3376da
dorkitude <kyle@kylewild.com>
2011-01-09 23:27:47 -0600

    better reporting in tests.sh

--- tests.sh
+++ tests.sh
@@ -1,18 +1,21 @@
-
-echo "testing PHP"
+#! /bin/bash
 
 echo "--------- begin PHP ---------" >> /var/log/tagalog.log
+echo "testing PHP..."
 php test.php
+echo "done!"
 echo "--------- endof PHP ---------" >> /var/log/tagalog.log
 
 echo "--------- begin Python ---------" >> /var/log/tagalog.log
-echo "testing Python"
+echo "testing Python..."
 python test.py
+echo "done!"
 echo "--------- begin Python ---------" >> /var/log/tagalog.log
 
 echo "--------- begin Ruby ---------" >> /var/log/tagalog.log
-echo "testing Ruby"
+echo "testing Ruby..."
 ruby test.rb
+echo "done!"
 echo "--------- begin Ruby ---------" >> /var/log/tagalog.log
 
 
