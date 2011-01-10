abac1795eeef66534152864bba78831ab6669729
dorkitude <kyle@kylewild.com>
2011-01-09 23:28:14 -0600

    fix test ending messages for ruby and python

--- tests.sh
+++ tests.sh
@@ -10,13 +10,13 @@ echo "--------- begin Python ---------" >> /var/log/tagalog.log
 echo "testing Python..."
 python test.py
 echo "done!"
-echo "--------- begin Python ---------" >> /var/log/tagalog.log
+echo "--------- endof Python ---------" >> /var/log/tagalog.log
 
 echo "--------- begin Ruby ---------" >> /var/log/tagalog.log
 echo "testing Ruby..."
 ruby test.rb
 echo "done!"
-echo "--------- begin Ruby ---------" >> /var/log/tagalog.log
+echo "--------- endof Ruby ---------" >> /var/log/tagalog.log
 
 
 
