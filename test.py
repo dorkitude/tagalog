a801e48c147d06a0f5df5e7e949d63bf05966ba9
dorkitude <kyle@kylewild.com>
2011-01-09 19:03:21 -0800

    test for python version

---
+++ test.py
@@ -0,0 +1,16 @@
+import tagalog
+
+tagalog.log('my message', ['nice', 'hello?'])
+
+my_dict = {
+  'chunky' : 'bacon!!!!!!!',
+  'what?' => 13511,
+}
+
+tagalog.log(my_dict, ['tag1', 'tag2'])
+
+
+tagalog.log(my_dict, 'off')
+
+
+tagalog.log('forced logging message', 'force')
