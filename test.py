8aa84d68ec4db03fa7efa56dc12cda2a9ed9c1df
dorkitude <kyle@kylewild.com>
2011-01-09 21:50:52 -0600

    comment the tests

--- test.py
+++ test.py
@@ -8,13 +8,16 @@ import tagalog
 # force a tag type exception
 # tagalog.log("message", {"unsupported" : "tag type"})
 
+
 # force a message type exception
 # import logging
 # tagalog.log(logging, 'tag')
 
 
+# test array of tags
 tagalog.log('my message', ['nice', 'hello?'])
 
+# test dictionary inputs
 my_dict = {
   'chunky' : 'bacon!!!!!!!',
   'what?' : 13511,
@@ -23,10 +26,11 @@ my_dict = {
 tagalog.log(my_dict, ['tag_3', 'tag_2', 'tag_1'])
 
 
+# test a tag that's off
 tagalog.log(my_dict, 'off')
 
-
+# test a tag that's on
 tagalog.log('forced logging message', 'force')
 
-
+# test default case
 tagalog.log('untagged message', 'untagged')
