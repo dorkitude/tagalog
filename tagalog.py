7352894f54e5192f6262066585497caced797082
dorkitude <kyle@kylewild.com>
2011-01-15 15:50:25 -0600

    remove printing from appengine python version, and make core python version not choke on new types

--- tagalog.py
+++ tagalog.py
@@ -1,94 +1,92 @@
-#   Usage:
-#       tagalog.log(Whatever_I_Want_To_Log, tagging);
+#     Usage:
+#             tagalog.log(Whatever_I_Want_To_Log, tagging);
 # 
-#       Where:
-#           Whatever_I_Want_To_Log    is something stringable, or a list of stringable things
-#       And:
-#           tagging    is either a tag string or a list of tag strings
+#             Where:
+#                     Whatever_I_Want_To_Log        is something stringable, or a list of stringable things
+#             And:
+#                     tagging        is either a tag string or a list of tag strings
 # 
 # 
-#   You can turn logging on or off for a given tag in the CONFIG AREA.
+#     You can turn logging on or off for a given tag in the CONFIG AREA.
 
 
 import datetime
 
 # THIS IS THE CONFIG AREA -- edit inline, or extend/override
 config = {
-  # this path can be relative or absolute
-  'log_file_path' : '/var/log/tagalog.log',
+    # this path can be relative or absolute
+    'log_file_path' : '/var/log/tagalog.log',
 
-  # set this to true if you want ALL logging turned off:
-  'kill_switch' : False,
-  
-  # turn logging on and off here for various tags:
-  'tags' : {
-    'tag_1' : True,
-    'tag_2' : True,
-    'tag_3' : True,
-    'off' : False,
-    'force' : True,
+    # set this to true if you want ALL logging turned off:
+    'kill_switch' : False,
     
-    # the default tag:
-    'untagged' : True, # this one is special
-  },
+    # turn logging on and off here for various tags:
+    'tags' : {
+        'tag_1' : True,
+        'tag_2' : True,
+        'tag_3' : True,
+        'off' : False,
+        'force' : True,
+        
+        # the default tag:
+        'untagged' : True, # this one is special
+    },
 }
 
 def log(message, tag_input='untagged'):
-  if config['kill_switch']:
-    return None
+    if config['kill_switch']:
+        return None
+        
+    message = format_message(message)
+    date_string = get_date_string()
+
+    loggable_tags = get_loggable_tags(tag_input)
+
+    log_file = open(config['log_file_path'], 'a')
     
-  message = format_message(message)
-  date_string = get_date_string()
-
-  loggable_tags = get_loggable_tags(tag_input)
-
-  log_file = open(config['log_file_path'], 'a')
-  
-  return_me = False
-  
-  for tag in loggable_tags:
-    write_me = "%s [ %s ] %s\n" % (date_string, tag, message)
-    log_file.write(write_me)
-    return_me = True
-  
-  log_file.close()
-  
-  return return_me
+    return_me = False
+    
+    for tag in loggable_tags:
+        write_me = "%s [ %s ] %s\n" % (date_string, tag, message)
+        log_file.write(write_me)
+        return_me = True
+    
+    log_file.close()
+    
+    return return_me
 
 def get_loggable_tags(tag_input):
-  if type(tag_input) == type(""):
-    tag_list = [tag_input]
-  elif type(tag_input) == type([]):
-    tag_list = tag_input
-  else:
-    raise TagalogException("Unsupported tag type. You must use a string or a list of strings (you sent %s )" % type(tag_input))
-  
-  return [tag for tag in tag_list if is_tag_loggable(tag)]
+    if type(tag_input) == type(""):
+        tag_list = [tag_input]
+    elif type(tag_input) == type([]):
+        tag_list = tag_input
+    else:
+        raise TagalogException("Unsupported tag type. You must use a string or a list of strings (you sent %s )" % type(tag_input))
+    
+    return [tag for tag in tag_list if is_tag_loggable(tag)]
 
 def is_tag_loggable(tag):
-  return (not config['tags'].has_key(tag)) or (config['tags'][tag])
-  
+    return (not config['tags'].has_key(tag)) or (config['tags'][tag])
+    
 
 def get_date_string():
-  time = datetime.datetime.now()
-  return "%s" % time
+    time = datetime.datetime.now()
+    return "%s" % time
 
 def format_message(message):
-  if type(message) == type("hi"):
+    try:
+        message = str(message)
+    except:
+        raise TagalogException("Unsupported message type %s. Casting as a string failed." % type(message))
+    
     return message
-  elif type(message) == type([]):
-    return message
-  elif type(message) == type({}):
-    return message
-  
-  else:
-    raise TagalogException("Unsupported message type. Currently only string, list, and dictionary are supported (you sent %s )" % type(message))
-  
+        
+    
 
 class TagalogException (Exception):
-  pass
-  
-  
+    pass
+    
+    
 # <license stuff>
 # 
 # tagalog is licensed under The MIT License
--- tagalog_appengine.py
+++ tagalog_appengine.py
@@ -2,19 +2,20 @@ from tagalog import *
 import logging
 
 def log(message, tag_input='untagged'):
-  if config['kill_switch']:
-    return None
     
-  message = format_message(message)
-  date_string = get_date_string()
+    if config['kill_switch']:
+        return None
+        
+    message = format_message(message)
+    date_string = get_date_string()
 
-  loggable_tags = get_loggable_tags(tag_input)
+    loggable_tags = get_loggable_tags(tag_input)
 
-  return_me = False
-  
-  for tag in loggable_tags:
-    write_me = "%s [ %s ] %s\n" % (date_string, tag, message)
-    logging.info(write_me)
-    return_me = True
+    return_me = False
     
-  return return_me
+    for tag in loggable_tags:
+        write_me = "%s [ %s ] %s" % (date_string, tag, message)
+        logging.info(write_me)
+        return_me = True
+        
+    return return_me
